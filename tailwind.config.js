e6effa8fe2cb4a05850b868fd67f017638311f53
lauchasalas <lauchasalas@gmail.com>
2025-08-05 18:57:36 +0800

    feat: parallax hero y dots en carrusel restaurados

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,12 @@
 module.exports = {
   darkMode: 'media',
   content: [
-    "./src/**/*.{njk,html,js}",
-    "./src/css/**/*.css"
+    "./src/**/*.{njk,html,js}"
   ],
   theme: {
     extend: {
-      keyframes: {
-        ripple: {
-          '0%': { transform: 'scale(0)', opacity: '0.5' },
-          '100%': { transform: 'scale(2.5)', opacity: '0' },
-        }
-      },
-      animation: {
-        ripple: 'ripple 0.6s ease-out',
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui']
       }
     }
   },
